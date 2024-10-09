ca3b1e0c2e50f16041d818402aadc5608654fdab
oxhunt <daniele.cacciabue@outlook.com>
2024-10-09 10:24:17 +0200

    up

--- FluidosIntegration/FLUIDOS_full_setup.sh
+++ FluidosIntegration/FLUIDOS_full_setup.sh
@@ -12,16 +12,20 @@ if [ "$1" != "edge" ] && [ "$1" != "robot" ] && [ "$1" != "delete" ]; then
 fi
 
 if [ "$1" == "delete" ]; then
+  
+  cd fluidos-chart
+  ./deploy.sh delete
+  cd ..
+  ./FLUIDOS_setup.sh delete
+  
+  # removing existing peers to allow for liqo to be uninstalled
   peers=$(kubectl get -n liqo foreignclusters.discovery.liqo.io | tail -n +2 | awk '{print $1}')
   echo "current peers: $peers"
   for peer in $peers; do
     echo "deleting peer $peer"
     liqoctl unpeer $peer --skip-confirm
   done
-  cd fluidos-chart
-  ./deploy.sh delete
-  cd ..
-  ./FLUIDOS_setup.sh delete
+  
   liqoctl uninstall --verbose --purge --skip-confirm
   exit 0
 else
