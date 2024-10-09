d77c1ef914c20786b487b1d8cea83a26d792924f
oxhunt <daniele.cacciabue@outlook.com>
2024-10-09 13:15:27 +0200

    update

--- FluidosIntegration/FLUIDOS_full_setup.sh
+++ FluidosIntegration/FLUIDOS_full_setup.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Exit on error
+set -e
+
+# Function to check if liqoctl is up and running
+check_liqoctl_status() {
+  while kubectl get -n liqo deployments.apps | tail -n +2 | awk '{print $2, $3, $4}' | grep -q 0; do
+    echo "Waiting for Liqo to be up and running..."
+    sleep 5
+  done
+}
+
+
+
 # Check for command-line argument
 if [ -z "$1" ]; then
   echo "Error: Missing argument. Use 'edge' to install the Edge node, 'robot' to install the Robot node or delete to delete the Helm deployment."
@@ -30,13 +43,12 @@ if [ "$1" == "delete" ]; then
   exit 0
 else
   liqoctl install k3s --cluster-name $(hostname)
-  kubectl rollout status -n liqo deployment/liqo-gateway
-  kubectl rollout status -n liqo deployment/liqo-controller-manager
-  kubectl rollout status -n liqo deployment/liqo-proxy
-  kubectl rollout status -n liqo deployment/liqo-crd-replicator
-  kubectl rollout status -n liqo deployment/liqo-auth
+
+  check_liqoctl_status
+  sleep 5
 
   ./FLUIDOS_setup.sh apply $1
+  sleep 5
   cd fluidos-chart
   ./deploy.sh $1
   exit 0
